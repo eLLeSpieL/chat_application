47d811fd39515cdd96c357e234cb8b76cc91eb5c
eLLeSpieL <p.e.espiel@gmail.com>
2023-09-06 00:02:08 +0800

    Added ChatFeed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () =>{
@@ -9,8 +11,8 @@ const App = () =>{
             projectID="7d57ae65-2aac-4db8-8260-8ea379f6a11e"
             userName="Elle"
             userSecret="123123"
+            renderChatFeed={(chatAppProps) =>  <ChatFeed  {... chatAppProps} />}
         />
-
     )
 }
 
