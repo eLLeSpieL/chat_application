1e3f8d1eec17f430a02f3e4474362c2da6d3053b
eLLeSpieL <p.e.espiel@gmail.com>
2023-09-06 02:03:40 +0800

    Fixed Login functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () =>{
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return(
         <ChatEngine 
             height="100vh"
             projectID="7d57ae65-2aac-4db8-8260-8ea379f6a11e"
-            userName="Elle"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) =>  <ChatFeed  {...chatAppProps} />}
         />
     )
