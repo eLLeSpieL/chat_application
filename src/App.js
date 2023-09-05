5a55f28f07d1b1c1f4cc184cd0efc11ee708a0e5
eLLeSpieL <p.e.espiel@gmail.com>
2023-09-05 23:52:31 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () =>{
+    return(
+        <ChatEngine 
+            height="100vh"
+            projectID="7d57ae65-2aac-4db8-8260-8ea379f6a11e"
+            userName="Elle"
+            userSecret="123123"
+        />
+
+    )
+}
+
+export default App;
